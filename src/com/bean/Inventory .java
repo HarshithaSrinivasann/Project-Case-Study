0257f984b99a8cf1c12b62f58af1c661d6073e17
Arup ku subudhi <48057404+Arup-ku-subudhi@users.noreply.github.com>
2022-09-08 17:09:17 +0530

    Add files via upload

---
+++ src/com/bean/Inventory .java
@@ -0,0 +1,60 @@
+package com.bean;
+
+import java.time.LocalDate;
+
+public class Inventory {
+		private String Type;
+		private int serial number;
+		private string macid;
+		private string Remote Control assetid;
+		private string Dish assetid;
+		private string Set TopBox Status;
+		
+		
+		public String getType() {
+			return Type ;
+		}
+		public void setType(String type) {
+			this.type = type;
+		}
+		public int getserial number() {
+			return serial number;
+		}
+		public void setserial number(int serial number ) {
+			this.serial number = serial number;
+		}
+		public string getmacid() {
+			return macidr;
+		}
+		public void setmacid(int macid ) {
+			this.macid = macid;
+		}
+		public string getRemote Control assetid() {
+			return Remote Control asset id;
+		}
+		public void setRemote Control asset id(int Remote Control asset id ) {
+			this.Remote Control asset id = Remote Control asset id;
+		}
+		public string getDish asset id() {
+			return Dish asset id;
+		}
+		public void setDish asset id(String Dish asset id) {
+			this.Dish asset id = Dish asset id;
+		}
+		public String getSet TopBox Status() {
+			return Set TopBox Status;
+		}
+		public void setSet TopBox Status(String Set TopBox Status) {
+			this.Set TopBox Status = Set TopBox Status;
+		}
+	}
+		
+public inventory(String Type,int serial number,string macid;string Remote Control asset id,string Dish asset id,string Set TopBox Status){
+	this.type=type;
+	this.number=number;
+	this.macid=macid;
+	this.Remote Control asset id=Remote Control asset id;
+	this.Dish asset id=Dish asset id;
+	this.Set TopBox Status=Set TopBox Status;
+	}
+
